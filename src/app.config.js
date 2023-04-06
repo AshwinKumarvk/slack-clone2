8622335be3c5ea145b58c67906ad9ef56c1c823f
AshwinKumarvk <vkashwinkumar20@gmail.com>
2023-04-06 11:53:20 +0530

    first commit

---
+++ src/app.config.js
@@ -0,0 +1,24 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJm7xr3ZTpq6K5FrFgNq43vWKS2JUe1c0",
+  authDomain: "slack-clone-78f4f.firebaseapp.com",
+  projectId: "slack-clone-78f4f",
+  storageBucket: "slack-clone-78f4f.appspot.com",
+  messagingSenderId: "657462540074",
+  appId: "1:657462540074:web:a5e11bc7f78a0d0b639053",
+  measurementId: "G-WTFCKRZ2VN"
+};
+
+const cometChat = {
+
+APP_ID: '236429144c25180e',
+
+AUTH_KEY: '8fa44362859ffa24d17287df25c09acb053bd532',
+
+REST_KEY: 'f3ce4f39700a8a6665e37cff43e1bc8005b655b9',
+
+APP_REGION: 'US',
+
+}
+
+export { firebaseConfig, cometChat }
+
